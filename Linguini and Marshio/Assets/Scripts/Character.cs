28e0c24a29be43e3283de74aba458f17d7e67a5b
DanielHolm999 <danielholm999@gmail.com>
2023-11-26 10:23:30 +0100

    needs enemies

---
+++ Linguini and Marshio/Assets/Scripts/Character.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    int currentHealth, maxhealth, currentExperience, maxExperience, currentlevel;
+    private LevelWindow levelWindow;
+
+    private void OnEnable()
+    {
+        ExperienceManager.Instance.onExperienceChange += HandleExperienceChange;
+    }
+
+    private void OnDisable()
+    {
+        ExperienceManager.Instance.onExperienceChange += HandleExperienceChange;
+    }
+
+    private void HandleExperienceChange(int newExperience)
+    {
+        currentExperience += newExperience;
+        if (currentExperience >= maxExperience)
+        {
+            LevelUp();
+        }
+    }
+
+    private void LevelUp()
+    {
+        maxhealth += 10;
+        currentHealth = maxhealth;
+        currentlevel++;
+        currentExperience = 0;
+        maxExperience += 100;
+        levelWindow.SetExperienceBarSize(currentExperience);
+        levelWindow.SetLevelNumber(currentlevel);
+    }
+
+    private void SetLevelWindow(LevelWindow levelWindow)
+    {
+        this.levelWindow = levelWindow;
+    }
+}
